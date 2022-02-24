000b5fb508c392483700ecd817b1666ae34c73bf
Semilore Idowu <semijacks27@gmail.com>
2022-02-24 13:38:24 +0100

    :sparkles: Add get report endpoint

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { data, ReportType } from './data';
+
+@Controller('report/:type')
+export class AppController {
+  @Get()
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  @Get(':id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'deleted';
+  }
+}
---
+++ src/data.ts
@@ -0,0 +1,44 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid',
+      source: 'Salary',
+      amount: 4000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'YouTube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
